30944adefdc7c27fb8f27d3444f3aeb28cc7093b
Aurore <aurore.queille@free.fr>
2017-03-17 15:23:00 +0100

    commit initial

---
+++ leSouk/src/main/java/com/miage/lesouk/LesoukApplication.java
@@ -0,0 +1,12 @@
+package com.miage.lesouk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LesoukApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LesoukApplication.class, args);
+	}
+}
---
+++ leSouk/src/test/java/com/miage/lesouk/LesoukApplicationTests.java
@@ -0,0 +1,16 @@
+package com.miage.lesouk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LesoukApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
