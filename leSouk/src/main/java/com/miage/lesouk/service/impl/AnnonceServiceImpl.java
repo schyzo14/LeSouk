678c2c36f83201953417fe8de0aa9cb1fcda8f62
Schyzo <>
2017-04-22 14:22:43 +0200

    Implémentation de toutes les méthodes de gestion des annonces

--- leSouk/src/main/java/com/miage/lesouk/service/impl/AnnonceServiceImpl.java
+++ leSouk/src/main/java/com/miage/lesouk/service/impl/AnnonceServiceImpl.java
@@ -1,61 +1,117 @@
 package com.miage.lesouk.service.impl;
 
 import com.miage.lesouk.entite.Annonce;
+import com.miage.lesouk.entite.Commentaire;
+import com.miage.lesouk.entite.Utilisateur;
 import com.miage.lesouk.repository.AnnonceRepository;
+import com.miage.lesouk.repository.CommentaireRepository;
+import com.miage.lesouk.repository.UtilisateurRepository;
 import com.miage.lesouk.service.AnnonceService;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AnnonceServiceImpl implements AnnonceService {
     
-/*    @Autowired
-    private AnnonceRepository annonceRepository;*/
+    @Autowired
+    private AnnonceRepository annonceRepository;
     
-    /*
+    @Autowired
+    private UtilisateurRepository utilisateurRepository;
+    
     @Autowired
     private CommentaireRepository commentaireRepository;
-    */
 
     @Override
     public Annonce getAnnonce(Integer idA) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return annonceRepository.findByIdA(idA);
     }
 
     @Override
     public List<Annonce> getAnnoncesCreees(Integer idUCreateur) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return annonceRepository.findByIdUCreateur(idUCreateur);
     }
 
     @Override
     public List<Annonce> getAnnoncesCandidatees(Integer idUCandidat) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return annonceRepository.findByIdUCandidat(idUCandidat);
     }
 
     @Override
-    public List<Annonce> getAnnonces(String motsCles) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Set<Annonce> getAnnonces(String motsCles) {
+        Set<Annonce> annonces = new TreeSet<>();
+        
+        for(Annonce a : annonceRepository.findByNomAContaining(motsCles)) {
+            annonces.add(a);
+        }
+        
+        for(Annonce a : annonceRepository.findByDescriptionAContaining(motsCles)) {
+            annonces.add(a);
+        }
+        
+        return annonces;
     }
 
     @Override
     public Annonce creerAnnonce(String nomA, String descriptionA, Long prixA, Integer idUCreateur) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Annonce annonce = new Annonce(nomA, descriptionA, prixA, idUCreateur);
+        Utilisateur utilisateur = utilisateurRepository.findById(idUCreateur);
+        
+        if(utilisateur.getAnnoncesCreees() == null) {
+            List<Annonce> annoncesCreees = new ArrayList<Annonce>();
+            utilisateur.setAnnoncesCreees(annoncesCreees);
+        }
+        utilisateur.getAnnoncesCreees().add(annonce);
+        
+        utilisateurRepository.save(utilisateur);
+        return annonceRepository.save(annonce);
     }
 
     @Override
     public Annonce candidaterAnnonce(Integer idA, Integer idUCandidat, Long prixCandidat) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Annonce annonce = annonceRepository.findByIdA(idA);
+        Utilisateur utilisateur = utilisateurRepository.findById(idUCandidat);
+        
+        annonce.setIdUCandidat(idUCandidat);
+        annonce.setPrixCandidat(prixCandidat);
+        annonce.setEtatA("Optionnée");
+        
+        if(utilisateur.getAnnoncesCandidatees() == null) {
+            List<Annonce> annoncesCandidatees = new ArrayList<Annonce>();
+            utilisateur.setAnnoncesCandidatees(annoncesCandidatees);
+        }
+        utilisateur.getAnnoncesCandidatees().add(annonce);
+        
+        utilisateurRepository.save(utilisateur);
+        return annonceRepository.save(annonce);
     }
 
     @Override
     public Annonce cloturerAnnonce(Integer idA) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Annonce annonce = annonceRepository.findByIdA(idA);
+        annonce.setEtatA("Cloturée");
+        return annonceRepository.save(annonce);
     }
 
     @Override
     public Annonce commenterAnnonce(Integer idA, Integer idUser, String texte) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Annonce annonce = annonceRepository.findByIdA(idA);
+        Date date = new Date();
+        Commentaire commentaire = new Commentaire(idA, idUser, texte, date);
+        
+        if(annonce.getListeCommentaires() == null) {
+            List<Commentaire> commentaires = new ArrayList<Commentaire>();
+            annonce.setListeCommentaires(commentaires);
+        }
+        annonce.getListeCommentaires().add(commentaire);
+        
+        commentaireRepository.save(commentaire);
+        return annonceRepository.save(annonce);
     }
     
 }
