1d9b58b60e39c357e6f07fc8daa60d25fd653743
Schyzo <>
2017-04-22 14:23:06 +0200

    Passage de la liste trouvée par Mot Clé en Set

--- leSouk/src/main/java/com/miage/lesouk/service/AnnonceService.java
+++ leSouk/src/main/java/com/miage/lesouk/service/AnnonceService.java
@@ -2,12 +2,13 @@ package com.miage.lesouk.service;
 
 import com.miage.lesouk.entite.Annonce;
 import java.util.List;
+import java.util.Set;
 
 public interface AnnonceService {
     public Annonce getAnnonce(Integer idA);
     public List<Annonce> getAnnoncesCreees(Integer idUCreateur);
     public List<Annonce> getAnnoncesCandidatees(Integer idUCandidat);
-    public List<Annonce> getAnnonces(String motsCles);
+    public Set<Annonce> getAnnonces(String motsCles);
     public Annonce creerAnnonce(String nomA, String descriptionA, Long prixA,
             Integer idUCreateur);
     public Annonce candidaterAnnonce(Integer idA, Integer idUCandidat, 
