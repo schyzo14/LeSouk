dda3e743e14e2aa290675de57bacb154c433fcab
Aurore <aurore.queille@free.fr>
2017-05-23 21:52:01 +0200

    > Ménage Commentaires

--- leSouk/src/main/java/com/miage/lesouk/entite/Commentaire.java
+++ leSouk/src/main/java/com/miage/lesouk/entite/Commentaire.java
@@ -11,6 +11,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
  */
 @Document(collection = "commentaires")
 public class Commentaire {
+    
     // Id du commentaire autogénéré
     @Id
     private String idC;
@@ -37,45 +38,84 @@ public class Commentaire {
         this.dateCreation = dateCreation;
     }
 
+    /**
+     * Récupère l'id du commentaire
+     * @return  id du commentaire
+     */
     public String getIdC() {
         return idC;
     }
 
+    /**
+     * Saisir l'id du commentaire
+     * @param idC   id du commentaire
+     */
     public void setIdC(String idC) {
         this.idC = idC;
     }
 
+    /**
+     * Récupère l'id de l'annonce
+     * @return  id de l'annonce
+     */
     public int getIdA() {
         return idA;
     }
 
+    /**
+     * Saisir l'id de l'annonce
+     * @param idA   id de l'annonce
+     */
     public void setIdA(int idA) {
         this.idA = idA;
     }
 
+    /**
+     * Récupère l'id de l'utilisateur
+     * @return  id de l'utilisateur
+     */
     public int getIdU() {
         return idU;
     }
 
+    /**
+     * Saisir l'id de l'utilisateur
+     * @param idU   id de l'utilisateur
+     */
     public void setIdU(int idU) {
         this.idU = idU;
     }
 
+    /**
+     * Récupère le texte
+     * @return  le texte
+     */
     public String getTexte() {
         return texte;
     }
 
+    /**
+     * Saisir le texte
+     * @param texte     le texte du commentaire
+     */
     public void setTexte(String texte) {
         this.texte = texte;
     }
 
+    /**
+     * Récupère la date de création du commentaire
+     * @return  date de création du commentaire
+     */
     public Date getDateCreation() {
         return dateCreation;
     }
 
+    /**
+     * Saisir la date de création du commentaire
+     * @param dateCreation  la date de création du commentaire
+     */
     public void setDateCreation(Date dateCreation) {
         this.dateCreation = dateCreation;
     }
-    
     
 }
