b9f8c08ce92177e5b1e24e79d77e654030879e30
Schyzo <Schyzo@Schyzo-PC>
2017-05-27 15:19:35 +0200

    Réparation CORS

--- leSouk/src/main/java/com/miage/lesouk/web/AnnonceController.java
+++ leSouk/src/main/java/com/miage/lesouk/web/AnnonceController.java
@@ -5,6 +5,7 @@ import com.miage.lesouk.entite.Commentaire;
 import com.miage.lesouk.service.AnnonceService;
 import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping("api/annonces")
 public class AnnonceController {
